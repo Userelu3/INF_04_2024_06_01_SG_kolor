1b0851c2314a9d8415dae0560453be1fda1c86cd
Userelu3 <krzysztof.ustowski@teb.edu.pl>
2026-02-04 14:10:19 +0100

    Liczenie wyniku kostek

--- INF_04_2024_06_01_SG_kolor/MainPage.xaml.cs
+++ INF_04_2024_06_01_SG_kolor/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+
     
         public MainPage()
         {
@@ -10,18 +11,31 @@
 
         private void ThrowButton_Clicked(object sender, EventArgs e)
         {
-            Random rnd = new Random();
-            int RandomNumber1 = rnd.Next(1, 7);
-            int RandomNumber2 = rnd.Next(1, 7);
-            int RandomNumber3 = rnd.Next(1, 7);
-            int RandomNumber4 = rnd.Next(1, 7);
-            int RandomNumber5 = rnd.Next(1, 7);
-            Dice1.Source = "k" + RandomNumber1 + ".jpg";
-            Dice2.Source = "k" + RandomNumber2 + ".jpg";
-            Dice3.Source = "k" + RandomNumber3 + ".jpg";
-            Dice4.Source = "k" + RandomNumber4 + ".jpg";
-            Dice5.Source = "k" + RandomNumber5 + ".jpg";
-            
+            Random rng = new Random();
+            int rng1 = rng.Next(1, 7);
+            int rng2 = rng.Next(1, 7);
+            int rng3 = rng.Next(1, 7);
+            int rng4 = rng.Next(1, 7);
+            int rng5 = rng.Next(1, 7);
+            Dice1.Source = "k" + rng1 + ".jpg";
+            Dice2.Source = "k" + rng2 + ".jpg";
+            Dice3.Source = "k" + rng3 + ".jpg";
+            Dice4.Source = "k" + rng4 + ".jpg";
+            Dice5.Source = "k" + rng5 + ".jpg";
+            int score = 0;
+            if (rng1 == rng2 || rng1 == rng3 || rng1 == rng4 || rng1 == rng5)
+                score += rng1;
+            if (rng2 == rng1 || rng2 == rng3 || rng2 == rng4 || rng2 == rng5)
+                score += rng2;
+            if (rng3 == rng1 || rng3 == rng2 || rng3 == rng4 || rng3 == rng5)
+                score += rng3;
+            if (rng4 == rng1 || rng4 == rng2 || rng4 == rng3 || rng4 == rng5)
+                score += rng4;
+            if (rng5 == rng1 || rng5 == rng2 || rng5 == rng3 || rng5 == rng4)
+                score += rng5;
+            PointsLabel.Text = "Wynik tego rzutu: " + score;
+
+
 
         }
 
